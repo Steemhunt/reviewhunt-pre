b3e1526303871785f4a7979ade81b8d9097904ac
Sebastian Kim <sydneyitguy@gmail.com>
2019-04-29 21:44:05 +0900

    Fix locale

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,8 @@
 class ApplicationController < ActionController::Base
   before_action :set_locale
 
-  def set_locale
-    locale = params[:locale] || extract_locale_from_accept_language_header
-    I18n.locale = I18n.available_locales.map(&:to_s).include?(locale) ? locale : 'en'
-  end
-
   private
-    def extract_locale_from_accept_language_header
-      request.env['HTTP_ACCEPT_LANGUAGE'].to_s.scan(/^[a-z]{2}/).first
+    def set_locale
+      I18n.locale = I18n.available_locales.map(&:to_s).include?(params[:locale]) ? params[:locale] : 'en'
     end
 end
