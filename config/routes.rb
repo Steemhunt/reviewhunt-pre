9ed5c9a4b2707c78c77ee62141dfcc197525db24
Sebastian Kim <sydneyitguy@gmail.com>
2019-04-29 11:31:57 +0900

    Change route

--- config/routes.rb
+++ config/routes.rb
@@ -3,18 +3,18 @@ Rails.application.routes.draw do
   resources :makers, only: [:new, :create]
   resources :hunters, only: [:new, :create]
 
-  scope "/:locale" do
-    root 'home#index'
-
-    resources :makers, only: [:new, :create]
-    resources :hunters, only: [:new, :create]
-  end
-
   namespace :admin do
     resources :channels
     resources :hunters
     resources :makers
 
     root to: "hunters#index"
+  end
+
+  scope "/:locale" do
+    root 'home#index'
+
+    resources :makers, only: [:new, :create]
+    resources :hunters, only: [:new, :create]
   end
 end
