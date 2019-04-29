e6501758cfe449125d488a8f3c12310397c66c44
Sebastian Kim <sydneyitguy@gmail.com>
2019-04-29 14:16:40 +0900

    Aggregate hunter stats

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,10 +1,14 @@
 class HomeController < ApplicationController
   def index
+    group_count = Channel.group(:channel_name).sum(:score)
+    group_count['Blogs'] = group_count.delete('steemit').to_d + group_count.delete('medium').to_d
+    group_count['Other Local Channels'] = group_count.delete('others')
+
     @stats = {
       maker_count: Maker.count,
       total_budget: Maker.sum(:monthly_budget),
       last_makers: Maker.order(id: :desc).first(10),
-      channel_count: Channel.group(:channel_name).sum(:score)
+      channel_count: group_count
     }
 
     @stats[:max_count] = @stats[:channel_count].values.max.to_d
