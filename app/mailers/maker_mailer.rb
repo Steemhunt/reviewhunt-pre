c27202970b32a1861abf083735b4e49b01f46267
Sebastian Kim <sydneyitguy@gmail.com>
2019-05-08 03:24:44 +0900

    Translate email title

--- app/mailers/maker_mailer.rb
+++ app/mailers/maker_mailer.rb
@@ -2,6 +2,6 @@ class MakerMailer < ApplicationMailer
   def pre_sign_up
     @user = params[:user]
     I18n.locale = @user.language
-    mail(to: @user.email, subject: 'You have pre signed-up for Reviewhunt')
+    mail(to: @user.email, subject: I18n.t('email.title'))
   end
 end
