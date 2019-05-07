5a6c8897991fbcda156ce2cbb50369ebe3e2411e
Sebastian Kim <sydneyitguy@gmail.com>
2019-05-08 02:51:06 +0900

    Try different address

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -63,12 +63,11 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "reviewhunt-pre_#{Rails.env}"
 
+  config.action_mailer.default_url_options = { host: 'https://review.hunt.town' }
   config.action_mailer.perform_caching = false
-
+  config.action_mailer.perform_deliveries = true
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  config.action_mailer.default_url_options = { host: 'https://review.hunt.town' }
-  config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.default charset: 'utf-8'
@@ -76,7 +75,7 @@ Rails.application.configure do
     address: 'smtp.gmail.com',
     port: 587,
     domain: 'gmail.com',
-    user_name: 'admin@hunt.town',
+    user_name: 'steemhunt@gmail.com',
     password: ENV['EMAIL_PASSWORD'],
     authentication: :plain,
     enable_starttls_auto: true
