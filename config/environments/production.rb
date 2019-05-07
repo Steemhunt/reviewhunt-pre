72585807c5ce6dbf04f9b8eedc2c465136413ef4
Sebastian Kim <sydneyitguy@gmail.com>
2019-05-08 02:55:48 +0900

    Fix image path

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -74,8 +74,8 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     address: 'smtp.gmail.com',
     port: 587,
-    domain: 'gmail.com',
-    user_name: 'steemhunt@gmail.com',
+    domain: 'hunt.town',
+    user_name: 'admin@hunt.town',
     password: ENV['EMAIL_PASSWORD'],
     authentication: :plain,
     enable_starttls_auto: true
